870987d1ceaacf91130a0bbd21d4d7e5cfe4dc1c
JohnSalinas123 <johnsalinas257@gmail.com>
2022-09-24 15:52:59 -0700

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import {Routes, Route, Link } from "react-router-dom"
+
+
+export function App() {
+
+    return (
+
+        <div id="root-div">
+
+            <Routes>
+                
+            </Routes>
+
+        </div>
+
+
+    )
+
+
+}
