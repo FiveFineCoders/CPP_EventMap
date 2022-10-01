9ef24f7b2340f5e1ddd9336c004cc989378686b1
Vu Nguyen <vanvuvinguyen@gmail.com>
2022-09-30 20:01:13 -0700

    fix: change width and height view

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,63 +1,57 @@
-import React, { useRef, useEffect, useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useRef, useEffect, useState } from "react";
+import ReactDOM from "react-dom";
 //import dotenv from 'dotenv'
-import mapboxgl, { FullscreenControl } from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
-import Map from 'react-map-gl';
-import { JsxEmit } from 'typescript';
-import '../styles/map.css'
+import mapboxgl, { FullscreenControl } from "mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+import Map from "react-map-gl";
+import { JsxEmit } from "typescript";
+import "../styles/map.css";
 
 //dotenv.config();
- 
-mapboxgl.accessToken = '';
+
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoiam9obnNhbGluYXMxMjMiLCJhIjoiY2w4NnQ5ZTNoMDY2ZjQwcnd5a3NrMXMyMiJ9.72BrMdkWJYhbp1sunZuHtg";
 
 type CPPMapProps = {
-    longitude: Number,
-    latitude: Number,
-    zoom: Number
-}
+  longitude: Number;
+  latitude: Number;
+  zoom: Number;
+};
 
 // CPP Longitude and latitude
 //34.05775617645074, -117.82261244351792
 
-export const CPPMap = (): JSX.Element =>  {
+export const CPPMap = (): JSX.Element => {
+  const MapContainerRef = useRef(null);
+  const [latitudeVal, setLatitude] = useState(34.05775617645074);
+  const [longitudeVal, setLongitude] = useState(-117.82261244351792);
+  const [zoomVal, setZoom] = useState(16);
+  const [mapWidth, setMapWidth] = useState(1000);
+  const [mapHeight, setMapHeight] = useState(800);
 
-    const MapContainerRef = useRef(null);
-    const [latitudeVal, setLatitude] = useState(34.05775617645074);
-    const [longitudeVal, setLongitude] = useState(-117.82261244351792);
-    const [zoomVal, setZoom] = useState(16);
-    const [mapWidth, setMapWidth] = useState(1000);
-    const [mapHeight, setMapHeight] = useState(800);
+  const handleClick = () => {
+    var curMapWidth: number = mapWidth;
+    console.log(curMapWidth);
 
-    const handleClick = () => {
+    setMapWidth(curMapWidth + 100);
+    console.log(mapWidth);
+  };
 
-        var curMapWidth:number = mapWidth;
-        console.log(curMapWidth)
+  return (
+    <div>
+      <Map
+        initialViewState={{
+          longitude: longitudeVal,
+          latitude: latitudeVal,
+          zoom: zoomVal,
+          pitch: 50,
+        }}
+        style={{ width: "100vw", height: "100vh" }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+        //onRender={(event) => event.target.resize()}
+        onResize={(event) => event.target.resize()}
+      />
 
-        setMapWidth(curMapWidth + 100)
-        console.log(mapWidth)
-
-    }
-
-    return (
-
-        <div>
-            <Map
-                initialViewState={{
-                    longitude: longitudeVal,
-                    latitude: latitudeVal,
-                    zoom: zoomVal,
-                    pitch: 50
-                }}
-                style={{width: mapWidth, height: mapHeight}}
-                mapStyle="mapbox://styles/mapbox/streets-v9"
-                //onRender={(event) => event.target.resize()}
-                onResize={(event) => event.target.resize()}
-                
-
-            />
-            
-            <button onClick={handleClick}>Press</button>
-        </div>
-    )
-
-}
+      <button onClick={handleClick}>Press</button>
+    </div>
+  );
+};
