d023908e9d13bc9ad5373a5f741455fa0108da95
bmoya13 <71987706+bmoya13@users.noreply.github.com>
2022-10-02 20:03:13 -0700

    Delete moya-api directory

--- moya-api/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const memberRouter = require('./routes/members');
-app.get('/', (req, res) => {
-    res.send("Brandon Moya's HTTP API for A3")
-})
-app.use('/members', memberRouter);
-const port = process.env.PORT || 5000;
-app.listen(port, () => 'Server running on port $(port)');
