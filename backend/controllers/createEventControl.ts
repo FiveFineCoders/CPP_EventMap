6a6ec7b2021a0b5d33f8cfeb6500a11280ac1bf2
John Salinas <46495232+JohnSalinas123@users.noreply.github.com>
2022-10-17 21:01:02 -0700

    split up controller methods for testing

--- backend/controllers/createEventControl.ts
+++ backend/controllers/createEventControl.ts
@@ -1,17 +1,6 @@
 import express, { Request, Response } from 'express';
 import Event from '../models/event.model';
 
-export const getEvents = async (req: Request, res: Response) => {
-	const allEvents = await Event.find();
-
-	if (!allEvents) {
-		res.status(400).json();
-	}
-
-	console.log('Got events!');
-	res.status(200).json(allEvents);
-};
-
 export const createEvent = async (req: Request, res: Response) => {
 	const { name, startTime, endTime, room, building, description, username } = req.body;
 
