16caef59004c96e7048847e6ea0731eff12baeda
JohnSalinas123 <salinasjohn257@gmail.com>
2022-11-15 12:56:58 -0800

    prettier update, changed color of marker

--- frontend/src/components/CPPMap.tsx
+++ frontend/src/components/CPPMap.tsx
@@ -15,27 +15,16 @@ import axios from 'axios';
 
 // @ts-ignore
 // eslint-disable-next-line import/no-webpack-loader-syntax
-mapboxgl.workerClass = require("worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker").default;	// exclude mapbox GL JS from transpilation (use ES5 not ES6)
+mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default; // exclude mapbox GL JS from transpilation (use ES5 not ES6)
 // load worker class (increases bundle size and reduces rendering performance)
 
 mapboxgl.accessToken = `${process.env.REACT_APP_MAPBOXTOKEN}`;
-
-const mapBounds = [
-	[34.046946290299026, -117.82929433297194], // southwest cords
-	[34.062726505089216, -117.81097666171729], // northeast cords
-];
 
 const initialViewport = {
 	longitude: -117.82261244351792,
 	latitude: 34.05775617645074,
 	zoom: 16,
 	pitch: 50,
-};
-
-type CPPMapProps = {
-	longitude: Number;
-	latitude: Number;
-	zoom: Number;
 };
 
 type eventMarker = {
@@ -53,22 +42,13 @@ type eventMarker = {
 
 const testBounds = new mapboxgl.LngLatBounds([-73.9876, 40.7661], [-73.9397, 40.8002]);
 
-// CPP Longitude and Latitude
-//34.05775617645074, -117.82261244351792
-
-/*const MapMarkers = (): JSX.Element => {
-
-
-	
-}	// end MapMarkers const*/
-
 export const CPPMap = (): JSX.Element => {
 	const MapContainerRef = useRef(null);
 	const [eventCreate, setEventCreate] = useState(false);
 	const [activateEventForm, setActivateEventForm] = useState(false);
 	const [longitude, setLongitude] = useState(0);
 	const [latitude, setLatitude] = useState(0);
-	const [eventMarkerList, setEventMarkerList] = useState<eventMarker[]>([])	// initialize empty array
+	const [eventMarkerList, setEventMarkerList] = useState<eventMarker[]>([]); // initialize empty array
 
 	const [viewState, setViewState] = React.useState({
 		longitude: -117.82261244351792,
@@ -77,21 +57,23 @@ export const CPPMap = (): JSX.Element => {
 		pitch: 50,
 	});
 
-	useEffect(() => {	// function is called after rendering map
+	useEffect(() => {
+		// function is called after rendering map
 		getMarkers();
-	}, [])	// end useEffect
+	}, []); // end useEffect
 
-	const getMarkers = async () => {	// retreives all events from server and adds them to an array
+	const getMarkers = async () => {
+		// retrieves all events from server and adds them to an array
 
 		try {
-			const { data } = await axios.get('api/events');	// send get request to server
+			const { data } = await axios.get('api/events'); // send get request to server
 
-			data.forEach((marker: eventMarker) => {	// for loop inserts each event into array
-				setEventMarkerList(prevMarker => [...prevMarker, marker]);	// add new marker to end of array
+			data.forEach((marker: eventMarker) => {
+				// for loop inserts each event into array
+				setEventMarkerList((prevMarker) => [...prevMarker, marker]); // add new marker to end of array
 			});
-
-		}	// end try
-		catch(error) {
+		} catch (error) {
+			// end try
 			// end try
 			if (axios.isAxiosError(error)) {
 				console.log('Axios error: ', error.message);
@@ -100,11 +82,10 @@ export const CPPMap = (): JSX.Element => {
 				console.log(error);
 				return 'Unexpected general error';
 			}
-		}	// end catch
+		} // end catch
+	}; // end getMarkers const
 
-	}	// end getMarkers const
-
-
+	// Starting cords
 	//34.027805, -117.845633
 	//34.064494, -117.779088
 	return (
@@ -119,55 +100,60 @@ export const CPPMap = (): JSX.Element => {
 					[-117.845633, 34.027805],
 					[-117.779088, 34.064494],
 				]}
-				onClick={event => {
-
+				onClick={(event) => {
 					// create event
-					if (!eventCreate) {	// don't show event creation form if event create button is not clicked
+					if (!eventCreate) {
+						// don't show event creation form if event create button is not clicked
 						return;
 					}
 
-					console.log("event create is: " + eventCreate)
+					console.log('event create is: ' + eventCreate);
 
 					// set event's longitude and latitude to mouse's location on click
-					setLongitude(event.lngLat.lng)
-					setLatitude(event.lngLat.lat)
+					setLongitude(event.lngLat.lng);
+					setLatitude(event.lngLat.lat);
 
-					setActivateEventForm(true)	// show popup
+					setActivateEventForm(true); // show popup
 				}}
 			>
+				{eventMarkerList.map(
+					(
+						event, // for loop iterates through array to render/display markers on the map
+					) => (
+						<Marker
+							key={event._id}
+							longitude={event.longitude}
+							latitude={event.latitude}
+							color={'#1fe81c'}
+						/>
+					),
+				)}
 
-				{
-					eventMarkerList.map((event) => (	// for loop iterates through array to render/display markers on the map
-						<Marker key={event._id} longitude={event.longitude} latitude={event.latitude}/>
-					))
-				}
-				
 				<MapSidebar />
 				<GeolocateControl position='top-right' />
-				<FullscreenControl position='top-right' />
 				<NavigationControl position='top-right' />
 				<ScaleControl />
-				
-				<div id = "eventPopup">
-					<PopupForm 
-						isOpen = {activateEventForm} 
-						togglePopup = {setEventCreate} 
-						isEventFormActive = {setActivateEventForm} 
-						longitudeVal = {longitude} 
-						latitudeVal = {latitude}
-						setEventList = {setEventMarkerList}
+
+				<div id='eventPopup'>
+					<PopupForm
+						isOpen={activateEventForm}
+						togglePopup={setEventCreate}
+						isEventFormActive={setActivateEventForm}
+						longitudeVal={longitude}
+						latitudeVal={latitude}
+						setEventList={setEventMarkerList}
 					/>
 				</div>
-				
-				<div id="addEventIcon">
-          			<AiFillPlusCircle 
-              			size="70"
-              			onClick={event => {
-							setEventCreate(true)
-                			console.log("create event button tapped: round 4 fixed?")
-              			}}
-            		/>
-        		</div>
+
+				<div id='addEventIcon'>
+					<AiFillPlusCircle
+						size='70'
+						onClick={(event) => {
+							setEventCreate(true);
+							console.log('create event button tapped: round 4 fixed?');
+						}}
+					/>
+				</div>
 
 				<div id='addEventIcon'>
 					<AiFillPlusCircle
