80aa19c695ab5192a04e43d88edd8d1bebc52510
JohnSalinas123 <salinasjohn257@gmail.com>
2022-11-29 12:14:49 -0800

    changed field names to match db

--- frontend/src/components/EventList.tsx
+++ frontend/src/components/EventList.tsx
@@ -3,72 +3,72 @@ import axios from 'axios';
 import { isShorthandPropertyAssignment } from 'typescript';
 import shortid from 'shortid';
 
-
 type event = {
-	name: String;
-	startTime: Date;
-	endTime: Date;
-	room: String;
-	building: String;
-	description: String;
-    date: Date;
+	eventName: String;
+	eventStartTime: Date;
+	eventEndTime: Date;
+	eventRoom: String;
+	eventBuilding: String;
+	eventDescript: String;
+	date: Date;
 	username: String;
 	longitude: number;
 	latitude: number;
 };
 
 export const EventList = () => {
-    const [events, setEvents] = useState<event[]>([])
+	const [events, setEvents] = useState<event[]>([]);
 
-    useEffect(() => {
+	useEffect(() => {
 		// function is called after rendering map
 		getData();
 	}, []); // end useEffect
 
-    const getData = async () => {
-        try {
-            const { data } = await axios.get('api/events');
-            data.forEach((event: event) => {
+	const getData = async () => {
+		try {
+			const { data } = await axios.get('api/events');
+			data.forEach((event: event) => {
 				// for loop inserts each event into array
 				setEvents((prevEvent) => [...prevEvent, event]); // add new event to end of array
 			});
-            console.log(events)
-        } catch (err) {
-            console.error(err);
-        }
-    }
+			console.log(events);
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-    return (
-        <div className="container">
-            <h3 className="p-3 text-center">CPP Events List</h3>
-            <table className="table table-striped table-bordered">
-                <thead>
-                    <tr>
-                        <th>Date Created</th>
-                        <th>Username</th>
-                        <th>Event Name</th>
-                        <th>Building</th>
-                        <th>Room</th>
-                        <th>Date</th>
-                        <th>Start Time</th>
-                        <th>End Time</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {events && events.map(event =>
-                        <tr key={Math.floor(Math.random() * 1000+1)}>
-                            <td>{new Date(event.date).toLocaleDateString()}</td>
-                            <td>{event.username}</td>
-                            <td>{event.name}</td>
-                            <td>{event.building}</td>
-                            <td>{event.room}</td>
-                            <td>{new Date(event.startTime).toLocaleDateString()}</td>
-                            <td>{new Date(event.startTime).toLocaleTimeString()}</td>
-                            <td>{new Date(event.endTime).toLocaleTimeString()}</td>
-                        </tr>
-                        )}
-                </tbody>
-            </table>
-        </div>
-    );
-}
+	return (
+		<div className='container'>
+			<h3 className='p-3 text-center'>CPP Events List</h3>
+			<table className='table table-striped table-bordered'>
+				<thead>
+					<tr>
+						<th>Date Created</th>
+						<th>Username</th>
+						<th>Event Name</th>
+						<th>Building</th>
+						<th>Room</th>
+						<th>Date</th>
+						<th>Start Time</th>
+						<th>End Time</th>
+					</tr>
+				</thead>
+				<tbody>
+					{events &&
+						events.map((event) => (
+							<tr key={Math.floor(Math.random() * 1000 + 1)}>
+								<td>{new Date(event.date).toLocaleDateString()}</td>
+								<td>{event.username}</td>
+								<td>{event.eventName}</td>
+								<td>{event.eventBuilding}</td>
+								<td>{event.eventRoom}</td>
+								<td>{new Date(event.eventStartTime).toLocaleDateString()}</td>
+								<td>{new Date(event.eventStartTime).toLocaleTimeString()}</td>
+								<td>{new Date(event.eventEndTime).toLocaleTimeString()}</td>
+							</tr>
+						))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
